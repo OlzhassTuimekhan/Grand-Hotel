9c4bfdda9e361a5294e157d336510011b4066334
OlzhassTuimekhan <olzhasstuimekhan@mail.ru>
2025-04-19 16:16:17 +0500

    First Commit

--- app/src/main/java/kz/grand_hotel/StartingPage.kt
+++ app/src/main/java/kz/grand_hotel/StartingPage.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-
+//Hello
 class StartingPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
