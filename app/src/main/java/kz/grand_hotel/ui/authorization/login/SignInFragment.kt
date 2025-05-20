d09e669388d0746b33810bbff7ff89e446227712
OlzhassTuimekhan <olzhasstuimekhan@mail.ru>
2025-05-20 20:22:59 +0500

    New Commit

--- app/src/main/java/kz/grand_hotel/ui/authorization/login/SignInFragment.kt
+++ app/src/main/java/kz/grand_hotel/ui/authorization/login/SignInFragment.kt
@@ -43,6 +43,7 @@ class SignInFragment : Fragment() {
 
 //  Sign in
     //New Commit
+    //New Commit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
